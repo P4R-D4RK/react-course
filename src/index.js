b1f4850393e222702918a684544bca22cafa4eae
Luis Polina <luis.282001@gmail.com>
2022-10-04 18:52:23 -0500

    components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
- 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hello World</h1>)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  function add(x, y) {
+    return x + y;
+  }
+
+  return <h1>{add(5, 7)}</h1>;
+}
+
+root.render(
+  <>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+  </>
+);
