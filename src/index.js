2aa34de22554bd26cf005d8849d25a0622d691a5
Luis Polina <luis.282001@gmail.com>
2022-10-19 11:37:34 -0500

    Styles and component types

--- src/index.js
+++ src/index.js
@@ -3,30 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
 import { Button } from "./Button";
+import {TaskCard} from "./Task";
+import {Saludar} from './Saludar'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-  <Button text='Click me'/>
-  <Button text='Pay'/>
-  <Button text='' name='Alex'/>
-    {/* <UserCard
-      name="Luis Polina"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () { alert('Hello') }}
-    />
-
-    <UserCard 
-      name="Eduardo Perez"
-      amount={1000}
-      married={false}
-      points={[400, 12]}
-      address={{ street: "prados 432", city: "Aguascalientes" }}
-    /> */}
-
+    <TaskCard ready={true}/>
+    <Saludar/>
   </>
 );
