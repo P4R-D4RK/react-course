4c9e7dad977f5733c1a0ac27831b67c8fb7b92c8
Luis Polina <luis.282001@gmail.com>
2022-11-07 11:56:29 -0600

    Fetch API and Third Party Modules

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Greeting, UserCard } from "./Greeting";
-import Product, { Navbar } from "./Product";
-import { Button } from "./Button";
-import {TaskCard} from "./Task";
-import {Saludar} from './Saludar'
+import { Posts } from "./Posts";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <TaskCard ready={true}/>
-    <Saludar/>
-    <Button text="Saludar"/>
-
-    <form onSubmit={(e) => {
-      e.preventDefault()
-      alert('Enviado')
-    }}>
-      <h1>Registro de usuario</h1>
-      <button>Send</button>
-    </form>
+    <Posts/>
   </>
 );
