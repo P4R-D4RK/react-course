fa4423b692bef4d87d2878271fbc187a9b32db27
Luis Polina <luis.282001@gmail.com>
2022-11-11 23:38:32 -0600

    useState

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const user = [
-  {
-    id: 1,
-    name: "Ryan ray",
-    img: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Luis Polina",
-    img: "https://robohash.org/user2",
-  },
-  {
-    id: 3,
-    name: "Eduardo Romo",
-    img: "https://robohash.org/user3",
-  },
-];
+function Counter() {
+  const [mensaje, setMensaje] = useState('');
+
+  return (
+    <div>
+      <input onChange={e =>setMensaje(e.target.value)}/>
+      <button onClick={() => {
+        alert('El mensaje es: ' + mensaje)
+      }}>
+        Save
+      </button>
+    </div>
+  );
+}
 
 root.render(
   <>
-    {user.map((user, index) => {
-      return (
-        <div key={index}>
-          <h1>{user.name}</h1>
-          <img src={user.img} />
-        </div>
-      );
-    })}
+    <Counter />
   </>
 );
